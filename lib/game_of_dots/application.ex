21feb29c1269c12d1819e1b72edece28e53833dc
prajaktarods <prajaktarodrigues@gmail.com>
2019-10-16 08:33:29 -0400

    modified

--- lib/game_of_dots/application.ex
+++ lib/game_of_dots/application.ex
@@ -17,7 +17,7 @@ defmodule GameOfDots.Application do
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Memory.Supervisor]
+    opts = [strategy: :one_for_one, name: GameOfDots.Supervisor]
     Supervisor.start_link(children, opts)
   end
 
