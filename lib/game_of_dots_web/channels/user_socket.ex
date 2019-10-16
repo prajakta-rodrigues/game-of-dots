b9ce4c1fb2e9ecf16f3dad4ba6f8cd7b03c6f9d1
prajaktarods <prajaktarodrigues@gmail.com>
2019-10-16 07:54:08 -0400

    modified endpoint.ex

--- lib/game_of_dots_web/channels/user_socket.ex
+++ lib/game_of_dots_web/channels/user_socket.ex
@@ -1,9 +1,9 @@
-defmodule MemoryWeb.UserSocket do
+defmodule GameOfDotsWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", MemoryWeb.RoomChannel
-  channel "games:*", MemoryWeb.GamesChannel
+  # channel "room:*", GameOfDotsWeb.RoomChannel
+  channel "games:*", GameOfDotsWeb.GamesChannel
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
   # verification, you can put default assigns into
@@ -26,7 +26,7 @@ defmodule MemoryWeb.UserSocket do
   # Would allow you to broadcast a "disconnect" event and terminate
   # all active sockets and channels for a given user:
   #
-  #     MemoryWeb.Endpoint.broadcast("user_socket:#{user.id}", "disconnect", %{})
+  #     GameOfDotsWeb.Endpoint.broadcast("user_socket:#{user.id}", "disconnect", %{})
   #
   # Returning `nil` makes this socket anonymous.
   def id(_socket), do: nil
